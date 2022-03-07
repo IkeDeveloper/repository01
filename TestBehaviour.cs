6f3d5c2a350f7e58b0a00c6cf80b35e49f0bdc34
IkeDeveloper <98697721+IkeDeveloper@users.noreply.github.com>
2022-03-07 22:13:00 +0000

    Add files via upload

---
+++ TestBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBehaviour : MonoBehaviour
+{
+    public float thrust;
+ 
+    void Start()
+    {
+   
+        StatusBarHandler.SetStatusBarValue(0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.O))
+             StatusBarHandler.SetStatusBarValue(StatusBarHandler.GetStatusBarValue() + 0.5f*Time.deltaTime);
+
+        else if (Input.GetKeyUp(KeyCode.O) && (StatusBarHandler.GetStatusBarValue() > .65f))
+              StatusBarHandler.SetStatusBarValue(.65f);
+
+        if (Input.GetKey(KeyCode.I))
+            StatusBarHandler.SetStatusBarValue(StatusBarHandler.GetStatusBarValue() - 0.1f * Time.deltaTime);
+        thrust = StatusBarHandler.GetStatusBarValue() * 1.5f;
+        Debug.Log(thrust);
+    }
+}
